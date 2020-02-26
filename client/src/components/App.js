918e99e0280bee204e39d0625287ca82f9a507e8
Roman Stix <rsonroad@gmail.com>
2020-02-26 19:00:56 +0100

    router used in the app

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import { BrowserRouter, Router } from 'react-router-dom';
+import { BrowserRouter, Route, Link   } from 'react-router-dom';
+
+const PageOne = () => {
+  return <div>
+    Page One
+    <Link to="/page-two">PageTwo</Link>
+    </div>
+}
+
+const PageTwo = () => {
+  return <div>Page Two
+    <Link to="/">PageOne</Link>
+  </div>
+}
 
 const App = () => {
-  return <div>App</div>;
+  return <div>
+    <BrowserRouter>
+      <div>
+        <Route path='/' exact component={PageOne} />
+        <Route path='/page-two' component={PageTwo} />
+      </div>
+    </BrowserRouter>
+  </div>;
 };
 
 export default App;
-
-// testing user1
+   
